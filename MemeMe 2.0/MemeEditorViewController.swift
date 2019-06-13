e2d80b5630a1ab272df06470e8dd4b88ed8bec38
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-13 12:48:15 +0300

    Added share and cancel navigation bar buttons

--- MemeMe 2.0/MemeEditorViewController.swift
+++ MemeMe 2.0/MemeEditorViewController.swift
@@ -14,10 +14,37 @@ class MemeEditorViewController: UIViewController {
     @IBOutlet var cameraButton: UIBarButtonItem!
     @IBOutlet var topTextField: UITextField!
     @IBOutlet var bottomTextField: UITextField!
-
+    @IBOutlet var navigationBar: UINavigationBar!
+    @IBOutlet var navigation: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        initAppSettings()
+    }
+
+    func initAppSettings() {
+        memeImage.image = nil
+        memeImage.contentMode = .scaleAspectFit
+
+        // Navigation bar buttons
+        navigation.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .cancel,
+            target: self,
+            action: #selector(cancelTapped))
+        navigation.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(shareItem))
+        
+    }
+
+    @objc func cancelTapped() {
+        print("Cancel")
+    }
+
+    @objc func shareItem() {
+        print("Share")
     }
     
 }
