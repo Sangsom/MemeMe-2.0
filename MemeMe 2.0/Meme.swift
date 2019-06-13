804ba1005471c0941a93d7164cd17ea12a00b745
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-13 11:31:03 +0300

    Added Meme file

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe 2.0
+//
+//  Created by Rinalds Domanovs on 13/06/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topString: String
+    var bottomString: String
+    var originalImage: UIImage
+    var updatedImage: UIImage
+}
