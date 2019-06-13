4624c4762396616804eb1cc9e32c268f72e3154a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-13 11:34:33 +0300

    Added outlets

--- MemeMe 2.0/MemeEditorViewController.swift
+++ MemeMe 2.0/MemeEditorViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class MemeEditorViewController: UIViewController {
 
+    @IBOutlet var memeImage: UIImageView!
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    @IBOutlet var topTextField: UITextField!
+    @IBOutlet var bottomTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
