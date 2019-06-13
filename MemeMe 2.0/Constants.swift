9c13399fb75dfc59d147d8c766520b7f89803a35
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-13 11:30:36 +0300

    Added Constants file

---
+++ MemeMe 2.0/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  MemeMe 2.0
+//
+//  Created by Rinalds Domanovs on 13/06/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+
+    struct TextField {
+        static let appName = "MemeMe"
+        static let topText = "TOP"
+        static let bottomText = "BOTTOM"
+        static let font = "HelveticaNeue-CondensedBlack"
+        static let size = 40
+        static let strokeColor = UIColor.black
+        static let foreGroundColor = UIColor.white
+        static let strokeWidth = -2.5
+    }
+
+}
